1da2053c92c6992fdd84f819e31ff95fa6441db5
abhaykohli <abhaykohli01@gmail.com>
2024-03-14 20:47:13 +0530

    Adding intial code

---
+++ Next.js/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const IndexPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to Next.js Component</h1>
+      <p>This is a simple TypeScript page.</p>
+    </div>
+  );
+};
+
+export default IndexPage;
---
+++ Wordpress/Index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>WordPress Demo Component</title>
+</head>
+<body>
+    <h1>Welcome to  My WordPress Component</h1>
+    <p>You have reached the first php page of MyWebApp.</p>
+</body>
+</html>
